a9ed68093bacaef690e3e68a08b220ce694af070
Dalepp888 <dalepp6@gmail.com>
2024-07-20 15:35:04 -0400

    nothing

--- src/components/sectionacerca.jsx
+++ src/components/sectionacerca.jsx
@@ -1,14 +1,15 @@
 import data from "@/api/data.json"
 
 export default function Acerca(){
+
+    const {info} = data
+
     return(
         <div className="max-sm:w-[500px] max-sm:m-auto">
             <h1 className="font-bold text-lg">Acerca de</h1>
-            {data.map(data => (
-                <div key={data.id}>
-                    <p className='py-3'>{data.info}</p>
+                <div>
+                    <p className='py-3'>{info}</p>
                 </div>
-            ))}
         </div>
     )
 }
