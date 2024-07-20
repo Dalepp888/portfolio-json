cbba6b90d840f09b6860f09e8143ab82405080cd
Dalepp888 <dalepp6@gmail.com>
2024-07-20 16:11:48 -0400

    add links

--- src/components/pila.jsx
+++ src/components/pila.jsx
@@ -1,12 +1,18 @@
-export default function Pila(){
-    return(
+import data from "@/api/data.json"
+
+export default function Pila() {
+
+    const { fotopila } = data
+
+    return (
         <div className="max-sm:w-[500px] max-sm:m-auto">
             <h1 className="font-bold text-lg">Pila Tecnológica</h1>
-            <div className="grid grid-cols-2 py-3">
-                <img src="/c++.png" className="border rounded-full w-20"/>
-                <img src="/django.png" className="border rounded-full w-20"/>
-                <img src="/php.png" className="border rounded-full w-20"/>
-                <img src="/phyton.png" className="border rounded-full w-20"/>
+            <div className="grid grid-cols-5 items-center">
+                {fotopila.map(fotopila => (
+                    <div className="py-3">
+                        <img src={fotopila} className="border rounded-full w-16" />
+                    </div>
+                ))}
             </div>
         </div>
     )
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,19 +1,22 @@
 import data from "@/api/data.json"
+import Link from "next/link"
 
-export default function Projects(){
-    
-    const {projects} = data
+export default function Projects() {
 
-    return(
+    const { projects } = data
+
+    return (
         <div>
             {projects.map(project => (
-                <div className="flex">
-                    <img src={project.img} className="w-[200px]"/>
-                    <div>
-                      <h1>{project.nombre}</h1>
-                      <p>{project.descripcion}</p>
+                <Link key={project.link} href={project.link}>
+                    <div className="flex py-5 hover:shadow-md rounded-md">
+                        <img src={project.img} className="w-[150px] p-2" />
+                        <div className="px-3 flex justify-center flex-col">
+                            <h1 className="font-bold text-pretty text">{project.nombre}</h1>
+                            <p>{project.descripcion}</p>
+                        </div>
                     </div>
-                </div>
+                </Link>
             ))}
         </div>
     )
