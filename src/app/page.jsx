6f88ca5324f0de7b611202ce557b20d9f4d19643
Dalepp888 <dalepp6@gmail.com>
2024-07-20 01:58:09 -0400

    portfolio json

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Head from '@/components/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='w-full'>
+      <Head/>
+    </div>
+  )
+}
---
+++ src/components/head.jsx
@@ -0,0 +1,8 @@
+export default function Head(){
+    return(
+        <div className="w-full">
+            <img src="/logos-u.png" alt="foto de portada" className=""/>
+            <img src="/logos-u.png" alt="foto de portada" className=""/>
+        </div>
+    )
+}
