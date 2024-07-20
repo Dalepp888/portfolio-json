1cae9955ba52921bf7ca6fd898eec3564e360456
Dalepp888 <dalepp6@gmail.com>
2024-07-20 15:39:32 -0400

    remove map

--- src/components/sectionEduca.jsx
+++ src/components/sectionEduca.jsx
@@ -1,15 +1,16 @@
 import data from "@/api/data.json"
 
 export default function Educa(){
+
+    const {carrera, escuela, fechaesco} = data
+
     return(
         <div className="max-sm:w-[500px] max-sm:m-auto">
             <h1 className="font-bold text-lg">Educaciòn</h1>
-            {data.map(data => (
-                <div className='py-3' key={data.id}>
-                    <h3>{data.carrera}</h3>
-                    <h3>{data.escuela} . {data.fechaesco}</h3>
+                <div className='py-3'>
+                    <h3>{carrera}</h3>
+                    <h3>{escuela} . {fechaesco}</h3>
                 </div>
-            ))}
         </div>
     )
 }
--- src/components/sectionexperien.jsx
+++ src/components/sectionexperien.jsx
@@ -1,45 +1,48 @@
 import data from'@/api/data.json'
 
 export default function Experiences(){
+
+  const {experience} = data
+
     return(
         <div className='max-sm:w-[550px] max-sm:m-auto'>
             <h1 className="font-bold text-lg">Experiencias</h1>
-            {data.map(data => (
-            <div className='max-sm:grid' key={data.id}>
+            {experience.map(experience => (
+            <div className='max-sm:grid'>
                 <div>
                     <div className='flex max-sm:grid max-sm:py-3 justify-between'>
                       <div>
-                        <h3 className='py-3 max-sm:py-1'>{data.experience1}</h3>
+                        <h3 className='py-3 max-sm:py-1'>{experience.name}</h3>
                       </div>
                       <div>
-                        <h3 className='py-3 max-sm:py-1'>{data.fecha1}</h3>
-                      </div>
-                    </div>
-
-                    <div className='flex max-sm:grid max-sm:py-3 justify-between'>
-                      <div>
-                        <h3 className='py-3 max-sm:py-1'>{data.experience2}</h3>
-                      </div>
-                      <div>
-                        <h3 className='py-3 max-sm:py-1'>{data.fecha2}</h3>
+                        <h3 className='py-3 max-sm:py-1'>{experience.fecha}</h3>
                       </div>
                     </div>
 
                     <div className='flex max-sm:grid max-sm:py-3 justify-between'>
                       <div>
-                        <h3 className='py-3 max-sm:py-1'>{data.experience3}</h3>
+                        <h3 className='py-3 max-sm:py-1'>{}</h3>
                       </div>
                       <div>
-                        <h3 className='py-3 max-sm:py-1'>{data.fecha3}</h3>
+                        <h3 className='py-3 max-sm:py-1'>{}</h3>
+                      </div>
+                    </div>
+
+                    <div className='flex max-sm:grid max-sm:py-3 justify-between'>
+                      <div>
+                        <h3 className='py-3 max-sm:py-1'>{}</h3>
+                      </div>
+                      <div>
+                        <h3 className='py-3 max-sm:py-1'>{}</h3>
                       </div>
                     </div>
 
                     <div className='flex justify-between max-sm:grid max-sm:py-3'>
                       <div>
-                        <h3 className='py-3 max-sm:py-1'>{data.experience4}</h3>
+                        <h3 className='py-3 max-sm:py-1'>{}</h3>
                       </div>
                       <div>
-                        <h3 className='py-3 max-sm:py-1'>{data.fecha4}</h3>
+                        <h3 className='py-3 max-sm:py-1'>{}</h3>
                       </div>
                     </div>
                 </div>
