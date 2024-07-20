3e743b3e033160e6f7d4e1d715c26acea37c219a
Dalepp888 <dalepp6@gmail.com>
2024-07-20 15:25:42 -0400

    cambio de info head

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,19 +1,20 @@
 import data from'@/api/data.json'
 
 export default function Head(){
+
+    const {name, algo, fecha} = data
+
     return(
         <div className="">
             <div className=''>
-              <img src="/logos-u.png" alt="foto de portada" className="w-screen h-[40vh] top-0 max-sm:w-screen max-sm:h-[40vh]"/>
-              <img src="/logos-p.png" alt="foto de portada" className="border rounded-full w-32 absolute top-48 left-1/2"/> 
+              <img src="/logos-u.png" alt="foto de portada" className="w-screen h-[40vh] top-0 max-sm:w-screen max-sm:h-[40vh]"/> 
             </div>
-            <div className='m-auto w-[300px] py-20 max-sm:w-[300px] max-sm:m-auto'>
-                {data.map(data => (
-                    <div key={data.id}>
-                        <h1 className='text-xl'>{data.name}</h1>
-                        <h3 className='text-slate-600'>{data.algo} . Creada {data.fecha}</h3>
+            <div className='m-auto w-[300px] py-20 max-sm:w-[300px] max-sm:m-auto relative'>
+            <img src="/logos-p.png" alt="foto de portada" className="border rounded-full w-32 absolute top-0 left-1/2"/>
+                    <div>
+                        <h1 className='text-xl'>{name}</h1>
+                        <h3 className='text-slate-600'>{algo} . Creada {fecha}</h3>
                     </div>
-                ))}
             </div>
         </div>
     )
