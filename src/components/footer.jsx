0347b54cdc625a8f0d1b3ed8d16430be30b08312
Dalepp888 <dalepp6@gmail.com>
2024-07-21 14:37:35 -0400

    create footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <div className="w-full py-5">
+            <div className="m-auto w-[280px]">
+                <p className="m-auto w-44">&copy; 2023 David Alejandro</p>
+                <p><span className="font-bold">Plantilla</span> creado por <span className="font-bold">David Alejandro</span></p>
+            </div>
+        </div>
+    )
+}
