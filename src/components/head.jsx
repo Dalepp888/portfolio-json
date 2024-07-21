256104fcb4a6ea5bc88b764242fea476c7622ac8
Dalepp888 <dalepp6@gmail.com>
2024-07-21 14:38:15 -0400

    edit names

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,20 +1,31 @@
-import data from'@/api/data.json'
+import data from '@/api/data.json'
+import Link from 'next/link'
 
-export default function Head(){
+export default function Head() {
 
-    const {name, algo, fecha} = data
+    const { name, user, fecha, cv, redes, perfil, portada } = data
 
-    return(
+    return (
         <div className="">
             <div className=''>
-              <img src="/logos-u.png" alt="foto de portada" className="w-screen h-[40vh] top-0 max-sm:w-screen max-sm:h-[40vh]"/> 
+                <img src={portada} alt="foto de portada" className="w-screen h-[40vh] top-0 max-sm:w-screen max-sm:h-[40vh]" />
             </div>
-            <div className='m-auto w-[300px] py-20 max-sm:w-[300px] max-sm:m-auto relative'>
-            <img src="/logos-p.png" alt="foto de portada" className="border rounded-full w-32 absolute top-0 left-1/2"/>
-                    <div>
-                        <h1 className='text-xl'>{name}</h1>
-                        <h3 className='text-slate-600'>{algo} . Creada {fecha}</h3>
+            <div className='m-auto w-[300px] py-20 text-center max-sm:w-[300px] max-sm:m-auto'>
+                <div className='w-full flex justify-center items-center relative'>
+                    <img src={perfil} alt="foto de portada" className="border bottom-10 w-20 h-20 rounded-full absolute" />
+                </div>
+                <div className=''>
+                    <h1 className='text-xl pb-2 font-semibold'>{name}</h1>
+                    <h3 className='text-slate-600 pb-2 text-sm'>{user} . Creada {fecha}</h3>
+                    <div className='flex justify-center pb-2'>
+                        {redes.map(red => (
+                            <Link href={red.link} className='rounded-full overflow-hidden w-6 h-6'>
+                                <img src={red.foto} className='w-full h-full object-cover' />
+                            </Link>
+                        ))}
                     </div>
+                    <Link href={cv} className='text-cyan-500 font-bold pb-2'>Descargar CV</Link>
+                </div>
             </div>
         </div>
     )
