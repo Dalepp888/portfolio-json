1460e164cfc703468442c3d30fead71f2a2b947b
Dalepp888 <dalepp6@gmail.com>
2024-07-21 19:11:27 -0400

    delete alt and border

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -8,11 +8,11 @@ export default function Head() {
     return (
         <div className="">
             <div className=''>
-                <img src={cover} alt="foto de portada" className="w-screen h-[40vh] top-0 max-sm:w-screen max-sm:h-[40vh]" />
+                <img src={cover} className="w-screen h-[40vh] top-0 max-sm:w-screen max-sm:h-[40vh]" />
             </div>
             <div className='m-auto w-[300px] py-20 text-center max-sm:w-[300px] max-sm:m-auto'>
                 <div className='w-full flex justify-center items-center relative'>
-                    <img src={profile} alt="foto de portada" className="border bottom-10 w-20 h-20 rounded-full absolute" />
+                    <img src={profile} className="bottom-10 w-20 h-20 rounded-full absolute" />
                 </div>
                 <div className=''>
                     <h1 className='text-xl pb-2 font-semibold'>{name}</h1>
