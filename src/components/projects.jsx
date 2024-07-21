cc20f931852c09130d5cdb5243bf26be6b0fdf0a
Dalepp888 <dalepp6@gmail.com>
2024-07-21 14:41:25 -0400

    edit look movile

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,4 +1,5 @@
 import data from "@/api/data.json"
+import Image from "next/image"
 import Link from "next/link"
 
 export default function Projects() {
@@ -6,13 +7,13 @@ export default function Projects() {
     const { projects } = data
 
     return (
-        <div>
+        <div className="flex flex-row flex-wrap p-1">
             {projects.map(project => (
                 <Link key={project.link} href={project.link}>
-                    <div className="flex py-5 hover:shadow-md rounded-md">
-                        <img src={project.img} className="w-[150px] p-2" />
-                        <div className="px-3 flex justify-center flex-col">
-                            <h1 className="font-bold text-pretty text">{project.nombre}</h1>
+                    <div className="flex max-lg:max-w-[300px] py-5 hover:shadow-md rounded-md max-lg:flex-col max-lg:">
+                        <Image src={project.img} className="p-2 flex max-lg:px-5 max-lg:flex max-lg:m-auto" width={200} height={200}/>
+                        <div className="px-3 flex  justify-center flex-col max-sm:w-[300px] max-lg:px-5">
+                            <h1 className="font-bold text">{project.nombre}</h1>
                             <p>{project.descripcion}</p>
                         </div>
                     </div>
