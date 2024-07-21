be14811540d501df8401a3288a0318da85fe3b19
Dalepp888 <dalepp6@gmail.com>
2024-07-21 14:44:04 -0400

    add see more

--- src/components/sectionexperien.jsx
+++ src/components/sectionexperien.jsx
@@ -1,53 +1,37 @@
-import data from'@/api/data.json'
+import data from '@/api/data.json'
+import Link from 'next/link'
 
-export default function Experiences(){
+export default function Experiences() {
 
-  const {experience} = data
+  const { experience } = data
+  const al = ">"
 
-    return(
-        <div className='max-sm:w-[550px] max-sm:m-auto'>
-            <h1 className="font-bold text-lg">Experiencias</h1>
-            {experience.map(experience => (
-            <div className='max-sm:grid'>
-                <div>
-                    <div className='flex max-sm:grid max-sm:py-3 justify-between'>
-                      <div>
-                        <h3 className='py-3 max-sm:py-1'>{experience.name}</h3>
-                      </div>
-                      <div>
-                        <h3 className='py-3 max-sm:py-1'>{experience.fecha}</h3>
-                      </div>
-                    </div>
-
-                    <div className='flex max-sm:grid max-sm:py-3 justify-between'>
-                      <div>
-                        <h3 className='py-3 max-sm:py-1'>{}</h3>
-                      </div>
-                      <div>
-                        <h3 className='py-3 max-sm:py-1'>{}</h3>
-                      </div>
-                    </div>
-
-                    <div className='flex max-sm:grid max-sm:py-3 justify-between'>
-                      <div>
-                        <h3 className='py-3 max-sm:py-1'>{}</h3>
-                      </div>
-                      <div>
-                        <h3 className='py-3 max-sm:py-1'>{}</h3>
-                      </div>
-                    </div>
-
-                    <div className='flex justify-between max-sm:grid max-sm:py-3'>
-                      <div>
-                        <h3 className='py-3 max-sm:py-1'>{}</h3>
-                      </div>
-                      <div>
-                        <h3 className='py-3 max-sm:py-1'>{}</h3>
-                      </div>
-                    </div>
+  return (
+    <div className=' max-sm:w-[400px] max-sm:px-5'>
+      <h1 className="font-bold text-lg">Experiencias</h1>
+      {experience.map(experience => (
+        <div className='max-sm:grid'>
+          <div>
+            <div className='flex max-sm:grid max-sm:py-3 justify-between py-3'>
+              <div className='flex'>
+                <div className='px-2'>
+                  <img src={experience.img} className='rounded-full w-16' />
                 </div>
+                <div className='flex flex-col'>
+                  <div className='py-1'>
+                    <h3 className='max-sm:py-1 font-bold'>{experience.ocu}</h3>
+                    <h3 className='text-slate-600'>{experience.name}</h3>
+                  </div>
+                  <Link href={experience.link} className='text-cyan-500 font-bold'>Saber màs {al}</Link>
+                </div>
+              </div>
+              <div className='max-sm:px-20'>
+                <h3 className='py-3 max-sm:py-1'>{experience.fecha}</h3>
+              </div>
             </div>
-        ))}
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  )
 }
