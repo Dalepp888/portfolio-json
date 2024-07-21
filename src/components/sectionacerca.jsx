f69ec8897d16d9af80485ab4c41ac001c9f8f878
Dalepp888 <dalepp6@gmail.com>
2024-07-21 14:42:07 -0400

    edit to lg

--- src/components/sectionacerca.jsx
+++ src/components/sectionacerca.jsx
@@ -5,7 +5,7 @@ export default function Acerca(){
     const {info} = data
 
     return(
-        <div className="max-sm:w-[500px] max-sm:m-auto">
+        <div className="max-lg:max-w-[350px] max-lg:px-5">
             <h1 className="font-bold text-lg">Acerca de</h1>
                 <div>
                     <p className='py-3'>{info}</p>
