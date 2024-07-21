adcd1cbaca5cb045eb6ee21e102f7bc5e7ef16f0
Dalepp888 <dalepp6@gmail.com>
2024-07-21 16:19:56 -0400

    created stack

---
+++ src/components/stack.jsx
@@ -0,0 +1,19 @@
+import data from "@/api/data.json"
+
+export default function Stack() {
+
+    const { photostack } = data
+
+    return (
+        <div className="max-sm:w-full max-sm:mx-5">
+            <h1 className="font-bold text-lg">Tech Stack</h1>
+            <div className="grid grid-cols-5 items-center max-sm:grid-cols-2">
+                {photostack.map(photostack => (
+                    <div className="py-3">
+                        <img src={photostack} className="rounded-full w-16" />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
