2e597b08ab6a44df012bf24d233e700f3a743d8e
Dalepp888 <dalepp6@gmail.com>
2024-07-22 00:53:29 -0400

    change footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,9 @@
+import Link from "next/link";
+
 export default function Footer() {
     return (
-        <div className="w-full py-5">
-            <div className="m-auto w-[300px]">
-                <p className="m-auto w-44">&copy; 2023 David Alejandro</p>
-                <p><span className="font-bold">Template</span> developed by <span className="font-bold">David Alejandro</span></p>
-            </div>
+        <div className="w-full py-5 text-center">
+            <p>Power By <Link href={"/"} className="font-bold">Template</Link></p>
         </div>
     )
 }
