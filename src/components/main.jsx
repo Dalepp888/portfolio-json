ac6233f55f25546bad763923378cc7df1cbbd4b5
Dalepp888 <dalepp6@gmail.com>
2024-07-22 01:04:22 -0400

    edit import

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,19 +1,19 @@
 import Stack from "./stack";
 import Projects from "./projects";
-import Acerca from "./sectionacerca";
-import Educa from "./sectionEduca";
-import Experiences from "./sectionexperien";
+import About from "./sectionabout";
+import Education from "./sectioneducation";
+import Experiences from "./sectionexperience";
 
 export default function Main(){
     return(
         <div className="w-full m-auto max-w-[700px] overflow-hidden">
-            <Acerca/>
+            <About/>
             <hr className="my-5"></hr>
             <Experiences/>
             <hr className="my-5"></hr>
             <Projects/>
             <hr className="my-5"></hr>
-            <Educa/>
+            <Education/>
             <hr className="my-5"></hr>
             <Stack/>
         </div>
